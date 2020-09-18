d0be52ff60605d41fbbaf82ea154a099c1ec9014
GeekaN <geekan@bk.ru>
2020-09-18 22:09:08 +0300

    Add some logs

--- src/modules/nats/index.js
+++ src/modules/nats/index.js
@@ -10,6 +10,8 @@ var collection;
   const connection = await MongoClient.connect(config.connection, { useUnifiedTopology: true, useNewUrlParser: true });
   const db = connection.db('albion');
   collection = db.collection('items_data');
+
+  console.log('Connected to mongodb', new Date());
 })();
 
 const cityCode = {
@@ -22,8 +24,16 @@ const cityCode = {
   4002: 'Fort Sterling',
 }
 
+let gotMessages = false;
+
 nc.subscribe('marketorders.deduped.bulk', async function (msg) {
   const response = JSON.parse(msg);
+
+  if (!gotMessages) {
+    gotMessages = true;
+    
+    console.log('Got some messages');
+  }
 
   for (let item of response) {
     if (!items.some(name => item.ItemTypeId.slice(3).includes(name)) || item.ItemTypeId.slice(1, 2) < 4 || !cityCode[item.LocationId]) {
